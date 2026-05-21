852f648b57b6578c1af0f6c9bbadcc2949d45b1d
Alex Brilevski <brilevski.alex@gmail.com>
2026-05-21 21:36:30 +0300

    Basic structure of Sign In page was implemented

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -2,7 +2,26 @@ import type { FC } from "react";
 
 const SignIn: FC = () => {
   return (
-    <div>Sign In</div>
+    <div>
+      <h1>Sign in to DevsNet</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" />
+        </div>
+        <div className="form-group">
+          <input id="remember-me" name="remember-me" type="checkbox" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <div className="form-actions">
+          <button>Sign in</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
